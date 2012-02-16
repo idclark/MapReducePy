872cb3eb2c0b51b88e61656061af816544b9066d
Ian Clark <idclark13@gmail.com>
2012-02-15 22:35:54 -0500

    comments for map

--- Map.py
+++ Map.py
@@ -1,11 +1,16 @@
 import sys
 
+#input from standard input 
 for line in sys.stdin:
 
     line = line.strip()
-
+    #remove white space
+    
     words = line.split()
-
+    #split line into individual words
+    
     for word in words:
-
+    #output here will become input for the reducer
+    #tab demiminated; min word count is 1
+        
         print word, 1
